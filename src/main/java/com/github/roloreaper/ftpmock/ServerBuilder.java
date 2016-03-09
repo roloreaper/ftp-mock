6319f1fce594dd45b72897f281f52fd185865327
Roloreaper <rolo.reaper@gmail.com>
2016-03-09 20:02:28 +0200

    Increased Coverage on Class

---
+++ src/main/java/com/github/roloreaper/ftpmock/ServerBuilder.java
@@ -0,0 +1,9 @@
+package com.github.roloreaper.ftpmock;
+
+/**
+ * Created by
+ * User :roloreaper
+ * Date :2016/03/09.
+ */
+public class ServerBuilder {
+}
---
+++ src/test/java/com/github/roloreaper/ftpmock/ServerBuilderTest.java
@@ -0,0 +1,30 @@
+package com.github.roloreaper.ftpmock;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by
+ * User :roloreaper
+ * Date :2016/03/09.
+ */
+public class ServerBuilderTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+    }
+}
